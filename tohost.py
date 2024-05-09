ea76c6b7614d81e1e01df846e21460a2895d13e2
Robert Tkach <119821797+roberttkach@users.noreply.github.com>
2024-05-10 03:57:13 +0900

    Update tohost.py

--- tohost.py
+++ tohost.py
@@ -10,7 +10,7 @@ def tohost(host, user, passw, frm, to):
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, username=user, password=passw)
         scp = SCPClient(ssh.get_transport())
-        remote_path = os.path.join(to, os.path.basename(frm)).replace("\\", "/")
+        remote_path = os.path.join(to, os.path.basename(frm))
         ssh.exec_command('rm -rf {}'.format(remote_path))
         scp.put(frm, recursive=True, remote_path=remote_path)
     finally:
@@ -18,5 +18,4 @@ def tohost(host, user, passw, frm, to):
         ssh.close()
 
 
-tohost('192.168.31.131', 'debian', '7492', r'C:\Users\diner\PycharmProjects\monitoring_ml\stream-data-machine',
-       '/home/debian/github')
+tohost('192.168.31.131', 'username', 'password', './stream-data-machine', '/home/debian/github')
