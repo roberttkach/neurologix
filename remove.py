521f2696c3f6aeab79ee3f0775b9a7e047a74cc0
Robert Tkach <dinerbone09@gmail.com>
2024-05-10 18:30:26 +0900

    Merge remote-tracking branch 'origin/main'

--- remove.py
+++ remove.py
@@ -7,15 +7,12 @@ def remove(host, user, passw):
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, username=user, password=passw)
 
-        # Завершить все контейнеры Docker
         stdin, stdout, stderr = ssh.exec_command("docker stop $(docker ps -a -q)")
         print(stdout.read().decode())
 
-        # Удалить все контейнеры Docker
         stdin, stdout, stderr = ssh.exec_command("docker rm $(docker ps -a -q)")
         print(stdout.read().decode())
 
-        # Удалить все изображения Docker
         stdin, stdout, stderr = ssh.exec_command("docker rmi $(docker images -a -q)")
         print(stdout.read().decode())
 
