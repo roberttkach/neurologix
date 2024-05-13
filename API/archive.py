a0e4c2d6a7d4c7e1bf8d846501967ee330b66328
Hallexz <yevtefeevah@gmail.com>
2024-05-13 12:32:51 +0700

    edit archive

--- API/archive.py
+++ API/archive.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, UploadFile, HTTPException, File
 from fastapi.responses import JSONResponse
 import shutil
-import os
+from pathlib import Path
 import zipfile
+
 
 app = FastAPI()
 
@@ -10,12 +11,17 @@ app = FastAPI()
 @app.post("/uploadzip/")
 async def upload_zip(file: UploadFile = File(...)):
     if file.filename.endswith(".zip"):
-        file_location = os.path.join("containers", file.filename)
+        some_condition = Path("containers").exists()
+        if some_condition:
+            file_location = Path("containers") / file.filename
+        else:
+            file_location = Path("/containers") / file.filename
+
         with open(file_location, 'wb+') as f:
             shutil.copyfileobj(file.file, f)
 
         with zipfile.ZipFile(file_location, 'r') as zip_ref:
-            zip_ref.extractall("containers")
+            zip_ref.extractall(Path("containers") / "your_project_directory")
 
         return JSONResponse(status_code=200, content={"message": "File uploaded and unzipped successfully"})
     else:
