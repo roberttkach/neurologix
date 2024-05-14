ef809119432924c3705fce3f37c1d0344f8db052
Robert Tkach <dinerbone09@gmail.com>
2024-05-14 13:39:53 +0900

    Merge remote-tracking branch 'origin/main'

--- API/archive.py
+++ API/archive.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 import shutil
@@ -27,51 +28,53 @@ def create_dockerfile(path: Path):
 async def upload(file: UploadFile = File(...), name: str = Form(...)):
     logging.info(f"Received file {file.filename}")
     if file.filename.endswith((".zip", ".7z", ".rar", ".tar.gz")):
-        dir_path = Path("neurologix/containers") / name
+        dir_path = Path("containers") / name
+        file_location = Path("cache") / file.filename
+        extract_location = dir_path
+
         if dir_path.exists():
             raise HTTPException(status_code=400, detail="Container with this name already exists.")
-        else:
-            file_location = Path("neurologix/cache") / file.filename
-            extract_location = dir_path
 
-            file_location.parent.mkdir(parents=True, exist_ok=True)
-            extract_location.mkdir(parents=True, exist_ok=True)
+        file_location.parent.mkdir(parents=True, exist_ok=True)
+        extract_location.mkdir(parents=True, exist_ok=True)
 
+        try:
             with open(file_location, 'wb+') as f:
                 shutil.copyfileobj(file.file, f)
 
-            try:
-                file_types = {
-                    ".zip": zipfile.ZipFile,
-                    ".7z": py7zr.SevenZipFile,
-                    ".rar": rarfile.RarFile,
-                    ".tar.gz": tarfile.open
-                }
+            file_types = {
+                ".zip": zipfile.ZipFile,
+                ".7z": py7zr.SevenZipFile,
+                ".rar": rarfile.RarFile,
+                ".tar.gz": tarfile.open
+            }
 
-                for ext, archive in file_types.items():
-                    if file.filename.endswith(ext):
-                        with archive(file_location, 'r') as arch:
-                            arch.extractall(path=extract_location)
-                            data_location = Path(extract_location) / file.filename
+            for ext, archive in file_types.items():
+                if file.filename.endswith(ext):
+                    with archive(file_location, 'r') as arch:
+                        arch.extractall(path=extract_location)
+                        data_location = Path(extract_location) / file.filename
+                        data_location = data_location.with_suffix('')
 
-                            if os.path.exists(data_location):
-                                files = os.listdir(data_location)
-                                for file in files:
-                                    shutil.move(os.path.join(data_location, file), extract_location)
+                        if os.path.exists(data_location):
+                            files = os.listdir(data_location)
+                            for file in files:
+                                old_path = os.path.join(data_location, file)
+                                new_path = os.path.join(extract_location, file)
+                                shutil.move(old_path, new_path)
+                                print(f"{old_path} >> {new_path}")
 
-                            print(extract_location)
+                            shutil.rmtree(data_location)
 
-                            required_files = ["requirements.txt", "train.py"]
-                            if not all(os.path.exists(os.path.join(extract_location, file)) for file in required_files):
-                                dir_content = os.listdir(extract_location)
-                                dir_content_str = ', '.join(dir_content)
-                                raise HTTPException(status_code=400,
-                                                    detail=f"{ext} file must contain requirements.txt and train.py. Current directory content: {dir_content_str}")
-                        break
-                else:
-                    raise HTTPException(status_code=400, detail="File type not supported.")
-            except Exception as e:
-                raise HTTPException(status_code=500, detail=str(e))
+                        required_files = ["requirements.txt", "train.py"]
+                        if not all(os.path.exists(os.path.join(extract_location, file)) for file in required_files):
+                            dir_content = os.listdir(extract_location)
+                            dir_content_str = ', '.join(dir_content)
+                            raise HTTPException(status_code=400,
+                                                detail=f"{ext} file must contain requirements.txt and train.py. Current directory content: {dir_content_str}")
+                    break
+            else:
+                raise HTTPException(status_code=400, detail="File type not supported.")
 
             create_dockerfile(extract_location)
 
@@ -82,5 +85,14 @@ async def upload(file: UploadFile = File(...), name: str = Form(...)):
             return JSONResponse(status_code=200,
                                 content={
                                     "message": f"File {name} uploaded, unzipped, and Docker image built successfully."})
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        finally:
+            try:
+                if file_location.exists():
+                    file_location.unlink()
+            except Exception as cleanup_error:
+                logging.error(f"Error during cleanup: {cleanup_error}")
     else:
         raise HTTPException(status_code=400, detail="Invalid file type. Please upload a zip, 7z, rar, or tar.gz file.")
+
