68d3035b4ccf5eb3cd5fcf3eaaa125ce28e526e5
Robert Tkach <dinerbone09@gmail.com>
2024-05-15 18:55:35 +0900

    Merge remote-tracking branch 'origin/main'

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY . .
 
 RUN pip install -r requirements.txt
 
-EXPOSE 8080
 EXPOSE 8000
+EXPOSE 9090
 
 CMD ["python3", "main.py"]
--- metrics/prometheus.py
+++ metrics/prometheus.py
@@ -8,8 +8,11 @@ uploaded_files_total = Counter('uploaded_files_total', 'Total uploaded files')
 
 
 def update_counter():
-    folders = [name for name in os.listdir('containers') if os.path.isdir(os.path.join('containers', name))]
-    uploaded_files_total.inc(len(folders))
+    if os.path.exists('containers'):
+        folders = [name for name in os.listdir('containers') if os.path.isdir(os.path.join('containers', name))]
+        uploaded_files_total.inc(len(folders))
+    else:
+        uploaded_files_total.inc(0)
 
 
 async def async_update_counter():
@@ -17,6 +20,5 @@ async def async_update_counter():
     with ThreadPoolExecutor() as pool:
         await loop.run_in_executor(pool, update_counter)
 
-
-start_http_server(8000)
+start_http_server(9090)
 asyncio.run(async_update_counter())
